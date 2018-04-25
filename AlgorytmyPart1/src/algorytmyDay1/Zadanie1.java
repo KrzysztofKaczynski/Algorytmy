53c7d3f8b682c52551f055c0f5bf714a706f35ae
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-25 13:11:10 +0200

    Pliki wer. 0.2

--- AlgorytmyPart1/src/algorytmyDay1/Zadanie1.java
+++ AlgorytmyPart1/src/algorytmyDay1/Zadanie1.java
@@ -1,4 +1,17 @@
-package algorytmyDay1;
+package algorytmyDay1;/*
+ *
+ *   "CWICZENIA Z LICZBAMI"
+ *
+ */
 
-public class Zadanie1 {
+public class Zadanie1
+
+//      1, 3, 5, 7, 9, 11... 61
+{
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i = i += 2) {
+            System.out.println(i + " ");
+        }
+    }
+
 }
