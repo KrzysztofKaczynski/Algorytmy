c24255361d5b8cbbff91a8c5ac33d68f77ce16de
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-25 13:24:02 +0200

    wer. 0.22

--- AlgorytmyPart1/src/algorytmyDay1/Zadanie1.java
+++ AlgorytmyPart1/src/algorytmyDay1/Zadanie1.java
@@ -1,17 +1,29 @@
-package algorytmyDay1;/*
- *
- *   "CWICZENIA Z LICZBAMI"
- *
+package algorytmyDay1;
+
+/**
+ * "CWICZENIA Z LICZBAMI"
  */
 
 public class Zadanie1
 
-//      1, 3, 5, 7, 9, 11... 61
+/**
+ *  wypisać   1, 3, 5, 7, 9, 11... 61
+ */
 {
     public static void main(String[] args) {
-        for (int i = 0; i < 6; i = i += 2) {
+        for (int i = 0; i < 61; i = i += 2) {
             System.out.println(i + " ");
         }
+        System.out.println("----------------------------");
+
+/**
+ *  wypisać   0, 2, 4, 6, 4, 2, 0
+ */
+
+        for (int i = 0; i <= 6 ; i+= 2) {
+            System.out.println(i + " ");
+        }
+        System.out.println("----------------------------");
     }
 
 }
