8653c76214d6f3b3648a0aae926cd9649e2feebf
[KrzysztofKaczyński] <qris70@gmail.com>
2018-04-27 15:04:05 +0200

    plus sortin i sortingsample

--- AlgorytmySortowaniePart2/src/algorytmySortDay2/NWWandNWD.java
+++ AlgorytmySortowaniePart2/src/algorytmySortDay2/NWWandNWD.java
@@ -3,30 +3,31 @@ package algorytmySortDay2;
 public class NWWandNWD {
 
     public static void main(String[] args) {
-        int a=12;
-        int b=10;
-        System.out.println("NWD: " + NWD(a,b));
-        System.out.println("NWW: " +  NWW(a,b));
+        int a = 12;
+        int b = 10;
+        System.out.println("NWD: " + NWD(a, b));
+        System.out.println("NWW: " + NWW(a, b));
     }
 
-    static int NWD(int a, int b){
-        while( a!=b ){
-            if(a>b)
-                a= a-b;
-            else
-                b= b-a;
-        }
-        return a;
-    }
-
-    static int NWW(int a, int b) {
-        int result =a*b;
+   public static int NWD(int a, int b) {
         while (a != b) {
-            if (a > b)
+            if (a > b) {
                 a = a - b;
-            else
+            } else {
                 b = b - a;
+            }
+            return a;
         }
-        return result/a;
     }
-}
+       public static int NWW( int a, int b) {
+            int result = a * b;
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                } else {
+                    b = b - a;
+                }
+                return result / a;
+            }
+        }
+    }
--- AlgorytmySortowaniePart2/src/algorytmySortDay2/Prime.java
+++ AlgorytmySortowaniePart2/src/algorytmySortDay2/Prime.java
@@ -5,17 +5,18 @@ import java.util.stream.IntStream;
 
 public class Prime {
     public static void main(String[] args) {
-        int number =11;
+        int number = 11;
         System.out.println(isPrime(number));
-        boolean isPrime = IntStream.range(2, (int)Math.sqrt(number))
-                .noneMatch(i -> number % i ==0);
+        boolean isPrime = IntStream.range(2, (int) Math.sqrt(number))
+                .noneMatch(i -> number % i == 0);
 
     }
-    static boolean isPrime(int n){
-        if(n <= 1)
+
+    static boolean isPrime(int n) {
+        if (n <= 1)
             return false;
-        for(int i=2; i<=Math.sqrt(n); i++) {
-            if(n % i ==0){
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
         }
